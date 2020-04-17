bfaf0db4c470c148016f4883edb1899b7fe31f65
OrangeVulture <fruitfuljoseph@gmail.com>
2020-04-17 12:38:43 +0530

    Initial Commit

---
+++ lib/categoriesScreen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'categoryItem.dart';
+import 'original.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                catData.id,
+                catData.title,
+                catData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 40,
+          mainAxisSpacing: 40,
+        ),
+      ),
+    );
+  }
+}
